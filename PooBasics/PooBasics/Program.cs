f34830e9161d44b2028c884baec30835820f5395
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2022-06-12 19:45:27 -0500

    Repository created

---
+++ PooBasics/PooBasics/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooBasics
+{
+    class Box
+    {
+        public int width;
+        public int length;
+        public int height;
+
+        public int Volume() => width * length * height;
+
+        //public static int CalculateVolume(Box box) =>
+        //    box.width* box.length* box.height;
+    }
+
+    internal class Program
+    {
+        //static int CalculateVolume(int width, int length, int height) =>
+        //    width * length * height;
+
+        //static int CalculateVolume(Box box) =>
+        //    box.width * box.length * box.height;
+
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+            boxes.Add(new Box() { width = 2, length = 1, height = 3 });
+            boxes.Add(new Box() { width = 1, length = 1, height = 3 });
+            boxes.Add(new Box() { width = 3, length = 5, height = 1 });
+
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine($"[{boxes[i].width},{boxes[i].length},{boxes[i].height}]");
+            }
+
+            Console.WriteLine();
+
+            int total = 0;
+            for (int i = 0; i < 3; ++i)
+            {
+                total += boxes[i].Volume();
+            }
+
+            Console.WriteLine($"Volumen total = {total}");
+        }
+
+        static void Main_1(string[] args)
+        {
+            Box box = new Box();
+            box.width = 1;
+            box.length = 2;
+            box.height = 3;
+
+            //int volume = box.width * box.length * box.height;
+            //int volume = CalculateVolume(box.width, box.length, box.height);
+            //int volume = CalculateVolume(box);
+            //int volume = Box.CalculateVolume(box);
+            int volume = box.Volume();
+            Console.WriteLine($"Volume: {volume}");
+        }
+    }
+}
---
+++ ValueOrReference/ValueOrReference/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueOrReference
+{
+    internal class Program
+    {
+        // paso por valor de variables tipo valor
+        static void Swap(int a, int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        // paso por referencia de variables tipo valor
+        static void Swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        class Box
+        {
+            public int x;
+            public int y;
+        }
+
+        // paso por valor de variables tipo referencia
+        static void Swap(Box a, Box b)
+        {
+            Box c = a;
+            a = b;
+            b = c;
+        }
+
+        // paso por referencia de variables tipo referencia
+        static void Swap(ref Box a, ref Box b)
+        {
+            Box c = a;
+            a = b;
+            b = c;
+        }
+
+        // ESPECIAL: paso por valor de variables tipo referencia
+        // Modificando el objeto y no la referencia
+        static void SwapSpecial(Box a, Box b)
+        {
+            int c = a.x;
+            a.x = b.x;
+            b.x = c;
+
+            c = a.y;
+            a.y = b.y;
+            b.y = c;
+        }
+
+        static void Main(string[] args)
+        {
+            Box a = new Box() { x = 10, y = 55 };
+            Box b = new Box() { x = 73, y = 42 };
+
+            Console.WriteLine($"a:({a.x},{a.y}), b:({b.x},{b.y})");
+
+            SwapSpecial(a, b);
+
+            Console.WriteLine($"a:({a.x},{a.y}), b:({b.x},{b.y})");
+        }
+
+        static void MainVal(string[] args)
+        {
+            int a = 10;
+            int b = 55;
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a={a}, b={b}");
+        }
+    }
+}

caa1a94f4b259bd205e3477ba106d4bd9f0d3883
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2022-06-20 19:33:09 -0500

    DentalCare project added

---
+++ DentalCare/DentalCare/Agenda.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace DentalCare
+{
+    internal class Agenda
+    {
+        private List<Day> days;
+        private List<Time> times;
+        private List<Schedule> schedules;
+        private List<Patient> patients;
+        private List<Appointment> appointments;
+
+        public Agenda()
+        {
+            days = EasyFile<Day>.LoadDataFromFile("days.txt",
+                    tokens => new Day(Convert.ToInt32(tokens[0]),
+                                      tokens[1]));
+
+            times = EasyFile<Time>.LoadDataFromFile("time.txt",
+                    tokens => new Time(Convert.ToInt32(tokens[0]),
+                                       tokens[1]));
+
+            schedules = EasyFile<Schedule>.LoadDataFromFile("schedule.txt",
+                    tokens => new Schedule(Convert.ToInt32(tokens[0]),
+                                           Convert.ToInt32(tokens[1])));
+
+            patients = EasyFile<Patient>.LoadDataFromFile("patients.txt",
+                    tokens => new Patient(Convert.ToInt32(tokens[0]),
+                                          tokens[1],
+                                          tokens[2]));
+
+            appointments = EasyFile<Appointment>.LoadDataFromFile("appointments.txt",
+                    tokens => new Appointment(Convert.ToInt32(tokens[0]),
+                                              Convert.ToInt32(tokens[1]),
+                                              Convert.ToInt32(tokens[2])));
+        }
+    }
+}
---
+++ DentalCare/DentalCare/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace DentalCare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+
+            int opcion = 0;
+            do
+            {
+                Clear();
+                WriteLine("**********************************");
+                WriteLine("*     AGENDA DENTAL-CARE 1.0     *");
+                WriteLine("*         menú principal         *");
+                WriteLine("**********************************");
+                WriteLine();
+                WriteLine("1. Consultas");
+                WriteLine("2. Citas");
+                WriteLine("0. Salir");
+
+                Write("\nElige una opcion: ");
+                opcion = Convert.ToInt32(ReadLine());
+
+                switch (opcion)
+                {
+                    case 0: // salir
+                        WriteLine("\n¡GRACIAS POR UTILIZAR EL PROGRAMA!");
+                        break;
+
+                    case 1:
+                        SubMenuConsultas();
+                        break;
+
+                    case 2:
+                        SubmenuCitas();
+                        break;
+
+                    default:
+                        WriteLine("\n¡OPCIÓN NO VÁLIDA!");
+                        ReadKey();
+                        break;
+                }
+            }
+            while (opcion != 0);
+        }
+
+        static void SubMenuConsultas()
+        {
+            int opcion = 0;
+            do
+            {
+                Clear();
+                WriteLine("**********************************");
+                WriteLine("*     AGENDA DENTAL-CARE 1.0     *");
+                WriteLine("*       submenú consultas        *");
+                WriteLine("**********************************");
+                WriteLine();
+                WriteLine("1. Citas por paciente");
+                WriteLine("2. Citas por día");
+                WriteLine("0. Volver");
+
+                Write("\nElige una opcion: ");
+                opcion = Convert.ToInt32(ReadLine());
+
+                switch (opcion)
+                {
+                    case 0: // volver
+                        break;
+
+                    case 1:
+                    case 2:
+                        WriteLine("\n¡OPCIÓN NO IMPLEMENTADA!");
+                        ReadKey();
+                        break;
+
+                    default:
+                        WriteLine("\n¡OPCIÓN NO VÁLIDA!");
+                        ReadKey();
+                        break;
+                }
+            }
+            while (opcion != 0);
+        }
+
+        static void SubmenuCitas()
+        {
+            int opcion = 0;
+            do
+            {
+                Clear();
+                WriteLine("**********************************");
+                WriteLine("*     AGENDA DENTAL-CARE 1.0     *");
+                WriteLine("*         submenú citas          *");
+                WriteLine("**********************************");
+                WriteLine();
+                WriteLine("1. Agendar");
+                WriteLine("2. Cancelar");
+                WriteLine("0. Volver");
+
+                Write("\nElige una opcion: ");
+                opcion = Convert.ToInt32(ReadLine());
+
+                switch (opcion)
+                {
+                    case 0: // volver
+                        break;
+
+                    case 1:
+                    case 2:
+                        WriteLine("\n¡OPCIÓN NO IMPLEMENTADA!");
+                        ReadKey();
+                        break;
+
+                    default:
+                        WriteLine("\n¡OPCIÓN NO VÁLIDA!");
+                        ReadKey();
+                        break;
+                }
+            }
+            while (opcion != 0);
+        }
+    }
+}
